0bcd615f29219e42a919834a25622614cbb5f0c8
YOU54F <yousafn@gmail.com>
2019-05-08 12:10:13 +0100

    remove test and build folders

--- build/lib/custom-responses.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function set(type: string, opts: {
-    url: string;
-    statusCode: any;
-    body: any;
-    headers: any;
-}): void;
-export declare function get(type: string, url: string): {}[];
-export declare function reset(type: string): void;
-export declare function resetAll(): void;
